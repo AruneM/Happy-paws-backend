d323e2cad2b9858023a03841d4a8a18d99ff2199
Manish Poduval <manishpoduval@hotmail.com>
2020-06-22 09:23:19 +0530

    final deployment 1.2

--- index.js
+++ index.js
@@ -47,12 +47,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()) //crucial for post requests from client
 
 
-app.use(express.static(path.join(__dirname, 'build')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use((req, res, next) => {
     // If no routes match, send them the React HTML.
-    res.sendFile(__dirname + "/build/index.html");
+    res.sendFile(__dirname + "/public/index.html");
   });
 
 //Register routes
