fde8e4eb6993159fddf4ec9f97dc0a966c4ca489
Manish Poduval <manishpoduval@hotmail.com>
2020-06-22 09:25:40 +0530

    final deployment 1.3

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()) //crucial for post requests from client
 
 
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use((req, res, next) => {
