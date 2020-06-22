90ba73febc94debef62de54eecbf220d936f1229
Manish Poduval <manishpoduval@hotmail.com>
2020-06-22 09:21:17 +0530

    final deployment 1.1

--- index.js
+++ index.js
@@ -65,6 +65,6 @@ app.use('/api', authRoutes);
 //Start the server to begin listening on a port
 // make sure you don't run it on port 3000 because 
 // your react app uses port 3000. 
-app.listen(process.env.PORT || 5000, '0.0.0.0', () => {
+app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
     console.log('Server is running')
 })
