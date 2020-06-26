62affa927bfa267379d82ef623c0f392e5941880
Arune Martinkeviciute <arunemart@gmail.com>
2020-06-26 12:22:41 +0200

    fixing github merged errors

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -77,69 +77,69 @@ router.post('/shelter/signup', (req, res) => {
 
 
 //AUTH ROUTER FOR USER
-router.post('/user/application', (req, res) => {
-  const {fullName, email, password, phone, job, livingPlace, have, availability} = req.body;
-  console.log(username, email, passwordHash);
+//router.post('/user/application', (req, res) => {
+  //const {fullName, email, password, phone, job, livingPlace, have, availability} = req.body;
+  // console.log(username, email, passwordHash);
   
-  // if any field is left empty
-  if (!fullName || !email || !password || !phone || !job || !livingPlace || !have || !availability) {
-      res.status(500)
-        .json({
-          errorMessage: 'Please fill in  all the fields'
-        });
-      return;  
-  }
+//   // if any field is left empty
+//   if (!fullName || !email || !password || !phone || !job || !livingPlace || !have || !availability) {
+//       res.status(500)
+//         .json({
+//           errorMessage: 'Please fill in  all the fields'
+//         });
+//       return;  
+//   }
 
-  const myRegex = new RegExp(/^[a-z0-9](?!.*?[^\na-z0-9]{2})[^\s@]+@[^\s@]+\.[^\s@]+[a-z0-9]$/);
-  if (!myRegex.test(email)) {
-      res.status(500)
-        .json({
-          errorMessage: 'Email format not correct'
-      });
-      return;  
-  }
+//   const myRegex = new RegExp(/^[a-z0-9](?!.*?[^\na-z0-9]{2})[^\s@]+@[^\s@]+\.[^\s@]+[a-z0-9]$/);
+//   if (!myRegex.test(email)) {
+//       res.status(500)
+//         .json({
+//           errorMessage: 'Email format not correct'
+//       });
+//       return;  
+//   }
 
-  const myPassRegex = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/);
-  if (!myPassRegex.test(passwordHash)) {
-    res.status(500)
-        .json({
-          errorMessage: 'Password needs to have 8 characters, a number and an Uppercase alphabet'
-        });
-      return;  
-  }
+//   const myPassRegex = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/);
+//   if (!myPassRegex.test(password)) {
+//     res.status(500)
+//         .json({
+//           errorMessage: 'Password needs to have 8 characters, a number and an Uppercase alphabet'
+//         });
+//       return;  
+//   }
 
-  bcrypt.genSalt(12)
-    .then((salt) => {
-      console.log('Salt: ', salt);
-      bcrypt.hash(password, salt)
-        .then((passwordHash) => {
-          User.model.create({fullname, email, passwordHash, phone, job, livingPlace, have, availability })
-            .then((user) => {
-              user.passwordHash = "***";
-              req.session.loggedInUser = user;
-              console.log(req.session)
-              res.status(200).json(user);
-            })
-            .catch((err) => {
-              if (err.code === 11000) {
-                res.status(500)
-                .json({
-                  errorMessage: 'name or email entered already exists!'
-                });
-                return;  
-              } 
-              else {
-                res.status(500)
-                .json({
-                  errorMessage: 'Something went wrong! Go to sleep!'
-                });
-                return; 
-              }
-            })
-        });  
-});
+//   bcrypt.genSalt(12)
+//     .then((salt) => {
+//       console.log('Salt: ', salt);
+//       bcrypt.hash(password, salt)
+//         .then((passwordHash) => {
+//           User.model.create({fullname, email, passwordHash, phone, job, livingPlace, have, availability })
+//             .then((user) => {
+//               user.passwordHash = "***";
+//               req.session.loggedInUser = user;
+//               console.log(req.session)
+//               res.status(200).json(user);
+//             })
+//             .catch((err) => {
+//               if (err.code === 11000) {
+//                 res.status(500)
+//                 .json({
+//                   errorMessage: 'name or email entered already exists!'
+//                 });
+//                 return;  
+//               } 
+//               else {
+//                 res.status(500)
+//                 .json({
+//                   errorMessage: 'Something went wrong! Go to sleep!'
+//                 });
+//                 return; 
+//               }
+//             })
+//         });  
+// });
 
-});
+// });
  
 router.post('/signin', (req, res) => {
     const {email, password } = req.body;
